b4f2d8a86dad47533f72cc1a3295129d7cd58b4c
bsegers <briansegs@gmail.com>
2026-05-13 09:43:59 -0400

    enhancement: Add animation to status

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -31,9 +31,12 @@ export function InputContainer({
       onSubmit={handleSubmit}
       className="flex flex-col gap-2 border rounded-lg p-2 relative"
     >
-      {loading && (
-        <div className="px-3 border rounded-lg absolute -top-8 left-1/2 -translate-x-1/2 text-slate-400 border-slate-300">
-          Generating response...
+      {true && (
+        <div className="px-3 items-baseline flex gap-1 border rounded-lg absolute -top-8 left-1/2 -translate-x-1/2 text-slate-400 border-slate-300">
+          <span>Generating response</span>
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce" />
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.15s]" />
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.3s]" />
         </div>
       )}
 
