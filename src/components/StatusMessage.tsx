f7c6a0b4ef4addeec401a129a33cffe0692e522c
bsegers <briansegs@gmail.com>
2026-05-15 18:18:51 -0400

    fix: Avoid showing typing animation in error state

--- src/components/StatusMessage.tsx
+++ src/components/StatusMessage.tsx
@@ -7,12 +7,16 @@ type StatusMessageProps = {
 
 export function StatusMessage({ error, status }: StatusMessageProps) {
   return (
-    <div className="px-3 items-baseline flex gap-1 border shadow absolute -top-8 left-1/2 -translate-x-1/2 text-primary bg-background">
-      {status === "streaming" && <span>Generating response</span>}
-      {status === "error" && <span>{error}</span>}
-      <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce" />
-      <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.15s]" />
-      <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.3s]" />
+    <div className="px-3 border shadow absolute -top-8 left-1/2 -translate-x-1/2 text-primary bg-background">
+      {status === "error" && <span>{error ?? "Something went wrong."}</span>}
+      {status === "streaming" && (
+        <div className="flex items-baseline gap-1">
+          <span>Generating response</span>
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce" />
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.15s]" />
+          <span className="h-1 w-1 rounded-full bg-slate-300 animate-bounce [animation-delay:0.3s]" />
+        </div>
+      )}
     </div>
   );
 }
